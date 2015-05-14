8e8a7d12803167fef3f5ff86410c515dd03e9f79
sumitp-optimus <sumit.pal.singh@optimusinfo.com>
2015-05-14 11:27:02 +0530

    adding functions

--- screenshot.js
+++ screenshot.js
@@ -1,33 +1,40 @@
-var obj = require('/home/local/OPTIMUSDOM/ubuntu90/Music/pixelcompare/Config.json');
-
-// var config = JSON.parse(obj);
-
+var config = require('/home/local/OPTIMUSDOM/ubuntu90/Music/pixelcompare/Config.json');
+var device = require('/home/local/OPTIMUSDOM/ubuntu90/Music/pixelcompare/Devices.json');
 var page = require('webpage').create();
-var l;
-// page.settings.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36';
 
+var snap = function (width, height, url){
+    page.open(url, function(status) {
+        console.log(url);
+        console.log(width);
+        console.log(height);
+        console.log(status);
+        page.render("hello.png");
+        // page.render(config.projectName"//input//screenshots//"page1.pageName".png");
+        phantom.exit();
+    });
+};
 
-page.open("http://www.headhonchos.com", function(status) {
-//     page.includeJs('https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js', function() {
-//         // https://cdnjs.cloudflare.com/ajax/libs/log4javascript/1.4.9/log4javascript.js
-//         page.evaluate(function() {
-//         l = document.getElementById('#authorize');
-//         console.log(l);
-//         // var $btn = ($('#authorize'));
-//         l.submit();
+// config.subject.forEach(function(page1){
+//     var url = page1.pageUrl;
+//     page1.devices.forEach(function(dev){
+//         var width = device[dev].width;
+//         var height = device[dev].height;
+//         snap(width, height, url);
 //     });
 // });
-//     console.log(l);
-    console.log(obj.projectName);
-    obj.subject.forEach(function(page){
-            console.log(page.pageName);
-        });
-    console.log(obj.page-url);
-    console.log('Status: ' + status);
-    console.log(page.title);
-    page.render(page.title+('.png'));
-    phantom.exit();
-});
+
+// snap(320, 2500, "http://www.headhonchos.com");
+for(var i = 0, page1 = config.subject; i < page1.length; i++) {
+    var url = page1[i].pageUrl;
+    // console.log(page1[i].devices.length);
+    for(var j=0, dev = page1[i].devices; j < dev.length; j++) {
+        // console.log("dev[j]----++++++" + dev[j]);
+        var dev1 = dev[j];
+        var width = device[dev1].width;
+        var height = device[dev1].height;
+        snap(width, height, url);
+    }
+}
 
 page.onError = function (msg, trace) {
     console.log(msg);
