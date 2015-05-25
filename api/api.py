780e64f7e5b53835576aaf5cd7696390b346e130
kesharwani_ankur <ankur.kesharwani001@gmail.com>
2015-05-25 16:31:20 +0530

    Code refarctoring, Adding api call for testcases.

--- api/api.py
+++ api/api.py
@@ -2,28 +2,84 @@ import sys
 import os
 import json
 from flask import make_response
-errors = {
-	0:"Error 1",
-	1:"Error 2",}
+
 
 def correct_path(path):
-	"""
-		Appends a / at the end of the path if not placed already.
-	"""
-	if path[len(path)-1] == '/':
-		return path
-	else:
-		return path + '/'
+    """
+    Appends a / at the end of the path if not placed already.
+    """
+    if path[len(path) - 1] == '/':
+        return path
+    else:
+        return path + '/'
 
 
 def readInputFile(file_name):
-	json_data = open(file_name).read()
-	data = json.loads(json_data)
-	return data
+    json_data = open(file_name).read()
+    data = json.loads(json_data)
+    return data
 
-def create_json_response(result):
-    """Convert result object to a JSON web response."""
+
+def create_json_response(status, error_code, message=None, data=None):
+    """
+	Convert result object to a JSON web response.
+	"""
+    result = None
+    if message is not None:
+        result = {
+            'status': status, 'error_code': error_code, 'message': message}
+    else:
+        result = {'status': status, 'error_code': error_code, 'data': data}
     response = make_response(json.dumps(result))
     response.headers['Access-Control-Allow-Origin'] = "*"
     response.mimetype = "application/json"
     return response
+
+
+def config_file_path(root, project_name, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/config.json'
+    else:
+        return project_name + '/config.json'
+
+
+def devices_file_path(root, project_name, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/devices.json'
+    else:
+        return project_name + '/devices.json'
+
+
+def source_file_path(root, project_name, page_name, device, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/input/source/' + page_name + '/' + device + '.png'
+    else:
+        return project_name + '/input/source/' + page_name + '/' + device + '.png'
+
+
+def screenshot_file_path(root, project_name, page_name, device, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/input/screenshots/' + page_name + '/' + device + '.png'
+    else:
+        return project_name + '/input/screenshots/' + page_name + '/' + device + '.png'
+
+
+def contourOnSource_file_path(root, project_name, page_name, device, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/output/' + page_name + '/' + device + '/contours_on_source.png'
+    else:
+        return project_name + '/output/' + page_name + '/' + device + '/contours_on_source.png'
+
+
+def contourOnSubject_file_path(root, project_name, page_name, device, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/output/' + page_name + '/' + device + '/contours_on_subject.png'
+    else:
+        return project_name + '/output/' + page_name + '/' + device + '/contours_on_subject.png'
+
+
+def diff_file_path(root, project_name, page_name, device, relative_path=False):
+    if relative_path is False:
+        return root + project_name + '/output/' + page_name + '/' + device + '/diff.png'
+    else:
+        return project_name + '/output/' + page_name + '/' + device + '/diff.png'
--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import api.prjinit as prjinit
 
 app = Flask(__name__)
 
+PRJ_ROOT = './static/projects/'
+
 @app.route("/")
 def index():
     return send_file("templates/index.html")
@@ -14,20 +16,95 @@ def index():
 def create_project():
     request_data = json.loads(request.data)
     config_data = request_data['config']
-    devices_data= request_data['devices']
-    resp_code = prjinit.create_project(config_data, devices_data, './projects')
+    devices_data = request_data['devices']
+    resp_code = prjinit.create_project(
+        config_data, devices_data, PRJ_ROOT)
     if resp_code == 0:
-        return api.create_json_response({"status":"success", "error_code":0, "message":"Project created."});
+        return api.create_json_response("success", 0, message="Project Created.")
     else:
-        return api.create_json_response({"status":"failure", "error_code":-1, "message":"Project cannot be created."});
+        return api.create_json_response("failure", -1, message="Project cannot be created.")
+
 
 @app.route("/projects", methods=['GET'])
 def get_projects():
-    projects = [];
-    for filename in os.listdir('./projects'):
-        if os.path.isdir(os.path.join('./projects', filename)):
+    projects = []
+    for filename in os.listdir(PRJ_ROOT):
+        if os.path.isdir(PRJ_ROOT + filename):
             projects.append(filename)
-    return api.create_json_response({"status":"success", "error_code":0, "projects":projects})
+    return api.create_json_response("success", 0, data=projects)
+
+
+@app.route("/testcases/<project_name>", methods=['GET'])
+def getTestCases(project_name):
+    config_data = api.readInputFile(api.config_file_path(PRJ_ROOT, project_name))
+    devices_data = api.readInputFile(api.devices_file_path(PRJ_ROOT, project_name))
+
+    response_data={
+        'projectName': config_data['projectName'],
+        'testcases': None
+    }
+    testcases=[]
+
+    for subject in config_data['subject']:
+        page_name=subject['pageName']
+        page_url=subject['pageUrl']
+        testcase={
+            'pageName': page_name,
+            'pageUrl': page_url,
+            'images': []
+        }
+        for device in subject['devices']:
+            image={
+                'device': device,
+                'source': None,
+                'screenshot': None,
+                'output': {
+                    'contourOnSubject': None,
+                    'contourOnSource': None,
+                    'diff': None
+                }
+            }
+
+            source_file_path=api.source_file_path(
+                PRJ_ROOT, project_name, page_name, device)
+            screenshot_file_path=api.screenshot_file_path(
+                PRJ_ROOT, project_name, page_name, device)
+            contourOnSource_file_path=api.contourOnSource_file_path(
+                PRJ_ROOT, project_name, page_name, device)
+            contourOnSubject_file_path=api.contourOnSubject_file_path(
+                PRJ_ROOT, project_name, page_name, device)
+            diff_file_path=api.diff_file_path(
+                PRJ_ROOT, project_name, page_name, device)
+
+            if(os.path.isfile(source_file_path)):
+                image['source']=api.source_file_path(
+                    PRJ_ROOT, project_name, project_name, device, True)
+            if(os.path.isfile(screenshot_file_path)):
+                image['screenshot']=api.screenshot_file_path(
+                    PRJ_ROOT, project_name, page_name, device, True)
+
+            if(os.path.isfile(contourOnSource_file_path)):
+                image['output']['contourOnSource']=api.contourOnSource_file_path(
+                    PRJ_ROOT, project_name, page_name, device, True)
+            if(os.path.isfile(contourOnSubject_file_path)):
+                image['output']['contourOnSubject']=api.contourOnSubject_file_path(
+                    PRJ_ROOT, project_name, page_name, device, True)
+            if(os.path.isfile(diff_file_path)):
+                image['output']['diff']=api.diff_file_path(
+                    PRJ_ROOT, project_name, page_name, device, True)
+
+            testcase['images'].append(image)
+        testcases.append(testcase)
+    response_data['testcases']=testcases
+    return api.create_json_response("success", 0, data=response_data)
+
+
+# @app.route("/upload/<project_name>", methods=['POST'])
+# def upload(project_name):
+#     request_data = json.loads(request.data)
+#     file_path = PRJ_ROOT + project_name + '/input/' +
+#         request_data['type'] + '/' + request_data['pageName']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
